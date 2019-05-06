dda85dc5fd60dffc32ce32ba20bd41aa150c6bd3
Peter Philips <pete@p373.net>
2019-05-06 16:31:01 -0700

    fix typo

--- ruby_unicode.rb
+++ ruby_unicode.rb
@@ -3,5 +3,5 @@ var1 = "From ruby: Mich\xc3\xa9lle"
 var2 = "From ruby: helløøééé"
 url = "https://gist.githubusercontent.com/synth/ce3b986ac543c2f92305262c57d093ad/raw/TestUnicode"
 
-puts var, var2
+puts var1, var2
 puts open(url){|f|f.read}
