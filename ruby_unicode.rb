c251eaae6eba26edfb4885f462dd4c7329d6a3f0
Peter Philips <pete@p373.net>
2019-05-06 16:23:31 -0700

    initial commit

---
+++ ruby_unicode.rb
@@ -0,0 +1,7 @@
+require 'open-uri'
+var1 = "From ruby: Mich\xc3\xa9lle"
+var2 = "From ruby: helløøééé"
+url = "https://gist.githubusercontent.com/synth/ce3b986ac543c2f92305262c57d093ad/raw/TestUnicode"
+
+puts var, var2
+puts open(url){|f|f.read}
